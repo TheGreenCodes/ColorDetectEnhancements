e73a46ffa4f15c3ebc3449a41d9f407f2febde9d
MarvinKweyu <mkweyu1@gmail.com>
2021-03-08 20:16:18 +0300

    chiore: initialize custom styling module

--- custom_styling.py
+++ custom_styling.py
@@ -1,1 +1,11 @@
 from colordetect import ColorDetect
+
+# parse the image.
+flowers = ColorDetect('./images/flowers.jpg')
+
+flowers.get_color_count()
+
+flowers.write_color_count(font_color=(0,0,255), top_margin=40)
+
+flowers.save_image(location='./images', file_name='processed.jpg')
+
